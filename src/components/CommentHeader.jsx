40693768a10cfcda2c5f8b75bab9b10f3c299656
g-akca <gok.akca66@gmail.com>
2026-04-29 20:59:09 +0300

    Add CommentHeader structure and styles

--- src/components/CommentHeader.jsx
+++ src/components/CommentHeader.jsx
@@ -1,7 +1,12 @@
+import avatar from "/images/avatars/image-amyrobson.png";
+
 function CommentHeader() {
   return (
-    <>
-    </>
+    <div className="flex gap-4 items-center text-[16px] leading-base">
+      <img src={avatar} alt="Avatar image" className="w-8 aspect-square rounded-full" />
+      <span className="text-gray-800 font-medium">amyrobson</span>
+      <span>1 month ago</span>
+    </div>
   )
 }
 
