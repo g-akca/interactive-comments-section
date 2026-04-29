7d5abc2cc5599523328af377929c44b639193027
g-akca <gok.akca66@gmail.com>
2026-04-29 21:51:15 +0300

    Remove unnecessary styles

--- src/components/CommentHeader.jsx
+++ src/components/CommentHeader.jsx
@@ -2,7 +2,7 @@ import avatar from "/images/avatars/image-amyrobson.png";
 
 function CommentHeader() {
   return (
-    <div className="flex gap-4 items-center text-[16px] leading-base">
+    <div className="flex gap-4 items-center">
       <img src={avatar} alt="Avatar image" className="w-8 aspect-square rounded-full" />
       <span className="text-grey-800 font-medium">amyrobson</span>
       <span>1 month ago</span>
