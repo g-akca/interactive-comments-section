16dea517e4ca3fe7eab6cef0794f8abea3a4d4f7
g-akca <gok.akca66@gmail.com>
2026-04-29 21:50:20 +0300

    Add VoteSection structure and styles

--- src/components/VoteSection.jsx
+++ src/components/VoteSection.jsx
@@ -1,6 +1,12 @@
+import plusIcon from "/images/icon-plus.svg";
+import minusIcon from "/images/icon-minus.svg";
+
 function VoteSection() {
   return (
-    <div className="bg-grey-50">
+    <div className="bg-grey-50 w-25 h-10 rounded-[10px] flex justify-center items-center gap-4">
+      <img src={plusIcon} alt="Plus icon" />
+      <span className="font-medium text-purple-600">12</span>
+      <img src={minusIcon} alt="Minus icon" />
     </div>
   )
 }
