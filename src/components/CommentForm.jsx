70c13ee8ac9a06ec5477a38e46cf11391afc078d
g-akca <gok.akca66@gmail.com>
2026-04-30 21:24:44 +0300

    Add CommentForm structure and styles

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,7 +1,15 @@
+import avatarImg from "/images/avatars/image-juliusomo.png";
+
 function CommentForm() {
   return (
-    <>
-    </>
+    <form className="bg-white px-4 py-[14.5px] rounded-lg flex flex-col gap-4">
+      <textarea placeholder="Add a comment..." className="px-4 py-2 h-24 border-grey-100 border rounded-lg resize-none placeholder:text-grey-500 focus:outline-none"></textarea>
+
+      <div className="flex justify-between items-center">
+        <img src={avatarImg} alt="Avatar image" className="w-8 aspect-square rounded-full" />
+        <button type="submit" className="bg-purple-600 w-26 h-12 rounded-lg flex justify-center items-center font-medium text-white uppercase">Send</button>
+      </div>
+    </form>
   )
 }
 
