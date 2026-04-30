e815a7495ae6c39e088b99b3c944b983b3da539c
g-akca <gok.akca66@gmail.com>
2026-04-30 22:13:06 +0300

    Use comments context in CommentList to show comments

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,13 +1,19 @@
+import { useComments } from "../context/CommentsContext";
 import CommentCard from "./CommentCard";
 import ReplySection from "./ReplySection";
 
 function CommentList() {
+  const { comments } = useComments();
+
   return (
     <div className="flex flex-col gap-4">
-      <CommentCard />
-
-      <CommentCard />
-      <ReplySection />
+      {comments.map(comment => (
+        comment.replies.length ? (
+          <ReplySection replies={comment.replies} />
+        ) : (
+          <CommentCard comment={comment} />
+        )
+      ))}
     </div>
   )
 }
