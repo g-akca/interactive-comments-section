d92572f345599d81cfb4bf54c15fb8a05688cfaa
g-akca <gok.akca66@gmail.com>
2026-04-30 21:44:39 +0300

    Add ReplySection structure and styles

--- src/components/ReplySection.jsx
+++ src/components/ReplySection.jsx
@@ -2,9 +2,14 @@ import CommentCard from "./CommentCard";
 
 function ReplySection() {
   return (
-    <>
-      <CommentCard />
-    </>
+    <div className="grid grid-cols-[2px_1fr] gap-4">
+      <div className="w-full bg-grey-100" />
+
+      <div className="flex flex-col gap-4">
+        <CommentCard />
+        <CommentCard />
+      </div>
+    </div>
   )
 }
 
