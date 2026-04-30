b9436a5c5e2c13506230893192e71dc85ac07003
g-akca <gok.akca66@gmail.com>
2026-04-30 12:25:33 +0300

    Add ReplyButton structure and styles

--- src/components/ReplyButton.jsx
+++ src/components/ReplyButton.jsx
@@ -1,7 +1,11 @@
+import replyIcon from "/images/icon-reply.svg";
+
 function ReplyButton() {
   return (
-    <>
-    </>
+    <button type="button" className="flex gap-2 items-center">
+      <img src={replyIcon} alt="Reply icon" className="w-3.5" />
+      <span className="text-purple-600 font-medium">Reply</span>
+    </button>
   )
 }
 
