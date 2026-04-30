4bc81d59025789357c460c070500dc3381fccd5a
g-akca <gok.akca66@gmail.com>
2026-04-30 22:24:40 +0300

    Pass replies to ReplySection

--- src/components/ReplySection.jsx
+++ src/components/ReplySection.jsx
@@ -1,13 +1,17 @@
 import CommentCard from "./CommentCard";
 
-function ReplySection() {
+function ReplySection({ replies }) {
   return (
     <div className="grid grid-cols-[2px_1fr] gap-4">
       <div className="w-full bg-grey-100" />
 
       <div className="flex flex-col gap-4">
-        <CommentCard />
-        <CommentCard />
+        {replies.map(reply => (
+          <CommentCard
+            key={reply.id}
+            comment={reply}
+          />
+        ))}
       </div>
     </div>
   )
