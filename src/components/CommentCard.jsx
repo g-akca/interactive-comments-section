5d0debc76d3897eb93a355f7bbbd1a448d4a9ca1
g-akca <gok.akca66@gmail.com>
2026-04-30 22:29:01 +0300

    Create isOwn bool in CommentCard to track if comment is own

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,7 +1,12 @@
+import { useComments } from "../context/CommentsContext";
 import CommentHeader from "./CommentHeader";
 import CommentActions from "./CommentActions";
 
 function CommentCard({ comment }) {
+  const { currentUser } = useComments();
+
+  const isOwn = comment.user.username === currentUser.username;
+
   return (
     <div className="bg-white rounded-lg p-4 flex flex-col gap-4">
       <CommentHeader createdAt={comment.createdAt} user={comment.user} />
