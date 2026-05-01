cdcc92dbc5cb8eaaca1274b3f1d414b4ec3f47c1
g-akca <gok.akca66@gmail.com>
2026-05-01 19:26:13 +0300

    Pass isOwn to CommentHeader, show badge

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -9,7 +9,7 @@ function CommentCard({ comment }) {
 
   return (
     <div className="bg-white rounded-lg p-4 flex flex-col gap-4">
-      <CommentHeader createdAt={comment.createdAt} user={comment.user} />
+      <CommentHeader createdAt={comment.createdAt} user={comment.user} isOwn={isOwn} />
 
       <p>{comment.content}</p>
 
--- src/components/CommentHeader.jsx
+++ src/components/CommentHeader.jsx
@@ -1,8 +1,15 @@
-function CommentHeader({ createdAt, user }) {
+function CommentHeader({ createdAt, user, isOwn }) {
   return (
     <div className="flex gap-4 items-center">
       <img src={user.image.png} alt="Avatar image" className="w-8 aspect-square rounded-full" />
-      <span className="text-grey-800 font-medium">{user.username}</span>
+
+      <div className="flex gap-2 items-center">
+        <span className="text-grey-800 font-medium">{user.username}</span>
+        {isOwn && (
+          <span className="w-9 h-4.75 bg-purple-600 rounded-xs flex justify-center items-center text-white font-medium lowercase text-[13px] leading-[120%]">You</span>
+        )}
+      </div>
+
       <span>{createdAt}</span>
     </div>
   )
