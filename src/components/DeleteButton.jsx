f4012b296fd4af3f0c6ecae1d1a4903f889e3c56
g-akca <gok.akca66@gmail.com>
2026-05-01 19:43:51 +0300

    Add DeleteButton structure and styles

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,7 +1,11 @@
+import deleteIcon from "/images/icon-delete.svg";
+
 function DeleteButton() {
   return (
-    <>
-    </>
+    <button type="button" className="flex gap-2 items-center">
+      <img src={deleteIcon} alt="Delete icon" className="h-3.5" />
+      <span className="text-pink-400 font-medium">Delete</span>
+    </button>
   )
 }
 
