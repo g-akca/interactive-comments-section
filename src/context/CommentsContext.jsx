dc825fdd7c53d29b782a85052b7219aea67ff64b
g-akca <gok.akca66@gmail.com>
2026-05-01 21:02:54 +0300

    Create addComment function for adding comments

--- src/context/CommentsContext.jsx
+++ src/context/CommentsContext.jsx
@@ -7,6 +7,36 @@ export function CommentsProvider({ children }) {
   const [comments, setComments] = useState(data.comments);
   const [currentUser, setCurrentUser] = useState(data.currentUser);
 
+  function addComment(content = "", createdAt = "now") {
+    function findMaxId(comments) {
+      let maxId = 0;
+
+      comments.forEach(comment => {
+        if (comment.id > maxId) maxId = comment.id;
+
+        if (comment.replies) {
+          comment.replies.forEach(reply => {
+            if (reply.id > maxId) maxId = reply.id;
+          });
+        }
+      });
+
+      return maxId;
+    }
+
+    setComments(prevComments => [
+      ...prevComments,
+      {
+        id: findMaxId(prevComments) + 1,
+        content,
+        createdAt,
+        score: 0,
+        user: currentUser,
+        replies: []
+      }
+    ]);
+  }
+
   function deleteComment(id) {
     function removeComment(list) {
       return list.filter(comment => comment.id !== id)
@@ -20,7 +50,7 @@ export function CommentsProvider({ children }) {
   }
   
   return (
-    <CommentsContext.Provider value={{ comments, currentUser, deleteComment }}>
+    <CommentsContext.Provider value={{ comments, currentUser, addComment, deleteComment }}>
       {children}
     </CommentsContext.Provider>
   )
