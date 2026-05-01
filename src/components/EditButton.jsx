614f9f5ef9804d3971c81343ebd2d289c4801b19
g-akca <gok.akca66@gmail.com>
2026-05-01 19:44:52 +0300

    Add EditButton structure and styles

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,8 +1,11 @@
+import editIcon from "/images/icon-edit.svg";
+
 function EditButton() {
   return (
-    <>
-    
-    </>
+    <button type="button" className="flex gap-2 items-center">
+      <img src={editIcon} alt="Edit icon" className="w-3.5" />
+      <span className="text-purple-600 font-medium">Edit</span>
+    </button>
   )
 }
 
