50cf525fe86f33ce366d6fc96a3ea1d3b79b7836
g-akca <gok.akca66@gmail.com>
2026-05-01 20:47:48 +0300

    Create deleteComment function for deleting comments

--- src/context/CommentsContext.jsx
+++ src/context/CommentsContext.jsx
@@ -6,9 +6,21 @@ const CommentsContext = createContext();
 export function CommentsProvider({ children }) {
   const [comments, setComments] = useState(data.comments);
   const [currentUser, setCurrentUser] = useState(data.currentUser);
+
+  function deleteComment(id) {
+    function removeComment(list) {
+      return list.filter(comment => comment.id !== id)
+        .map(comment => ({
+          ...comment, 
+          replies: removeComment(comment.replies || [])
+        }));
+    }
+
+    setComments(prevComments => removeComment(prevComments));
+  }
   
   return (
-    <CommentsContext.Provider value={{ comments, currentUser }}>
+    <CommentsContext.Provider value={{ comments, currentUser, deleteComment }}>
       {children}
     </CommentsContext.Provider>
   )
