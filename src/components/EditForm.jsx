291b330b6c533b4ece5d9fd0f4fb03ae51d095ed
g-akca <gok.akca66@gmail.com>
2026-05-03 11:08:22 +0300

    Add EditForm structure and styles

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,7 +1,22 @@
 function EditForm() {
   return (
-    <>
-    </>
+    <form className="flex flex-col gap-4">
+      <textarea 
+        placeholder="Edit your comment..." 
+        className="
+          px-4 py-2 h-24 border-grey-100 border rounded-lg resize-none placeholder:text-grey-500 
+          duration-150 focus:outline-none focus:border-purple-600
+        "
+      >
+      </textarea>
+
+      <button 
+        type="submit" 
+        className="self-end bg-purple-600 w-26 h-12 rounded-lg flex justify-center items-center font-medium text-white uppercase"
+      >
+        Update
+      </button>
+    </form>
   )
 }
 
