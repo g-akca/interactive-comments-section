856228789f6fb496ed5d729c4be6b720d9402edf
g-akca <gok.akca66@gmail.com>
2026-05-04 12:44:23 +0300

    Add tablet responsive DeleteModal styles

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,7 +1,7 @@
 function DeleteModal({ onCancel, onDelete }) {
   return (
     <div className="fixed inset-0 bg-black/50 p-4 flex justify-center items-center">
-      <div className="w-full max-w-100 bg-white px-[25.5px] py-[21.5px] rounded-lg flex flex-col gap-4">
+      <div className="w-full max-w-100 bg-white px-[25.5px] tablet:px-7.75 py-[21.5px] tablet:py-[27.5px] rounded-lg flex flex-col gap-4 tablet:gap-6">
         <h2 className="text-grey-800 font-medium text-[24px] leading-[120%]">Delete comment</h2>
         <p className="text-grey-500">Are you sure you want to delete this comment? This will remove the comment and can’t be undone.</p>
 
