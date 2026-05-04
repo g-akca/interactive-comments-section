f52815e9de9d0de7d213fda76d345a23778ab4a3
g-akca <gok.akca66@gmail.com>
2026-05-04 12:12:48 +0300

    Add tablet responsive CommentForm styles

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,6 +1,7 @@
 import { useComments } from "../context/CommentsContext";
 import { useState } from "react";
 import avatarImg from "/images/avatars/image-juliusomo.png";
+import React from "react";
 
 function CommentForm({ topId = 0, closeForm }) {
   const { addComment } = useComments();
@@ -16,20 +17,44 @@ function CommentForm({ topId = 0, closeForm }) {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="bg-white px-4 py-[14.5px] rounded-lg flex flex-col gap-4">
-      <textarea 
-        value={content}
-        onChange={e => setContent(e.target.value)}
-        placeholder="Add a comment..." 
-        className="
-          px-4 py-2 h-24 border-grey-100 border rounded-lg resize-none placeholder:text-grey-500 
-          text-grey-800 duration-150 focus:outline-none focus:border-purple-600
-        "
-      >
-      </textarea>
+    <form onSubmit={handleSubmit} className="bg-white px-4 py-[14.5px] tablet:p-6 rounded-lg">
+      <div className="flex flex-col gap-4 tablet:hidden">
+        <textarea 
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          placeholder="Add a comment..." 
+          className="
+            px-4 py-2 h-24 border-grey-100 border rounded-lg resize-none placeholder:text-grey-500 
+            text-grey-800 duration-150 focus:outline-none focus:border-purple-600
+          "
+        >
+        </textarea>
 
-      <div className="flex justify-between items-center">
-        <img src={avatarImg} alt="Avatar image" className="w-8 aspect-square rounded-full" />
+        <div className="flex justify-between items-center">
+          <img src={avatarImg} alt="Avatar image" className="w-8 aspect-square rounded-full" />
+          <button 
+            type="submit" 
+            className="bg-purple-600 w-26 h-12 rounded-lg flex justify-center items-center font-medium text-white uppercase"
+          >
+            {topId !== 0 ? "Reply" : "Send"}
+          </button>
+        </div>
+      </div>
+
+      <div className="hidden flex-row gap-4 items-start tablet:flex">
+        <img src={avatarImg} alt="Avatar image" className="w-10 aspect-square rounded-full" />
+
+        <textarea 
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          placeholder="Add a comment..." 
+          className="
+            grow px-4 py-2 h-24 border-grey-100 border rounded-lg resize-none placeholder:text-grey-500 
+            text-grey-800 duration-150 focus:outline-none focus:border-purple-600
+          "
+        >
+        </textarea>
+
         <button 
           type="submit" 
           className="bg-purple-600 w-26 h-12 rounded-lg flex justify-center items-center font-medium text-white uppercase"
