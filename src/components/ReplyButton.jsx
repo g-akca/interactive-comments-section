045dabd75cbcdf92b8357532bc4f564b94004f08
g-akca <gok.akca66@gmail.com>
2026-05-04 13:08:59 +0300

    Create ReplyIcon component, add hover style

--- src/components/ReplyButton.jsx
+++ src/components/ReplyButton.jsx
@@ -1,10 +1,10 @@
-import replyIcon from "/images/icon-reply.svg";
+import ReplyIcon from "./icons/ReplyIcon";
 
 function ReplyButton({ onClick }) {
   return (
-    <button type="button" onClick={onClick} className="flex gap-2 items-center">
-      <img src={replyIcon} alt="Reply icon" className="w-3.5" />
-      <span className="text-purple-600 font-medium">Reply</span>
+    <button type="button" onClick={onClick} className="flex gap-2 items-center text-purple-600 hover:text-purple-200 cursor-pointer transition-all">
+      <ReplyIcon />
+      <span className="font-medium">Reply</span>
     </button>
   )
 }
