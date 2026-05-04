1e48678eeb72bc7b27c270442ae607f053cc753a
g-akca <gok.akca66@gmail.com>
2026-05-04 13:00:43 +0300

    Create PlusIcon and MinusIcon components, give active styles

--- src/components/VoteSection.jsx
+++ src/components/VoteSection.jsx
@@ -1,25 +1,25 @@
-import plusIcon from "/images/icon-plus.svg";
-import minusIcon from "/images/icon-minus.svg";
 import { useComments } from "../context/CommentsContext";
+import PlusIcon from "./icons/PlusIcon";
+import MinusIcon from "./icons/MinusIcon";
 
 function VoteSection({ comment }) {
   const { voteComment } = useComments();
 
   return (
     <div className="bg-grey-50 w-25 tablet:w-10 h-10 tablet:h-25 px-2.25 tablet:px-1.25 tablet:py-[15.75px] rounded-[10px] flex tablet:flex-col justify-between items-center gap-4 tablet:shrink-0">
-      <img
-        src={plusIcon}
-        alt="Plus icon"
-        onClick={() => voteComment(comment.id, "up")}
-      />
+      <button type="button" onClick={() => voteComment(comment.id, "up")} className="cursor-pointer">
+        <PlusIcon 
+          className={`${comment.upVoted ? "text-purple-600" : "text-purple-200"} hover:text-purple-600 transition-all`}
+        />
+      </button>
 
       <span className="font-medium text-purple-600">{comment.score}</span>
 
-      <img
-        src={minusIcon}
-        alt="Minus icon"
-        onClick={() => voteComment(comment.id, "down")}
-      />
+      <button type="button" onClick={() => voteComment(comment.id, "down")} className="cursor-pointer">
+        <MinusIcon 
+          className={`${comment.downVoted ? "text-purple-600" : "text-purple-200"} hover:text-purple-600 transition-all`}
+        />
+      </button>
     </div>
   )
 }
