4a3e039f4c1299d88ab86f240a41141a81ed892d
g-akca <gok.akca66@gmail.com>
2026-08-12 16:55:19 +0300

    Remove alt text from avatar image in CommentHeader for improved accessibility

--- src/components/CommentHeader.jsx
+++ src/components/CommentHeader.jsx
@@ -9,7 +9,7 @@ function CommentHeader({ createdAt, user, isOwn, onReply, onDelete, onEdit }) {
   return (
     <div className="flex gap-4 justify-between">
       <div className="flex gap-4 items-center">
-        <img src={user.image.png} alt="Avatar image" className="w-8 aspect-square rounded-full" />
+        <img src={user.image.png} alt="" className="w-8 aspect-square rounded-full" />
 
         <div className="flex gap-2 items-center">
           <span className="text-grey-800 font-medium">{user.username}</span>
